9e4c5a81444475a7ee341f7d73ddabbbeb8615be
Justin Barabander <justinbarabander@Justins-MacBook-Pro.local>
2015-08-28 16:03:07 -0400

    Changed validation

--- public/js/controllers/newCard.controller.js
+++ public/js/controllers/newCard.controller.js
@@ -12,37 +12,9 @@ app.controller('NewCardController', function($scope, FlashCardsFactory) {
   $scope.master = angular.copy($scope.newCard);
   $scope.categories = FlashCardsFactory.categories;
   $scope.reset = function(){
-    angular.copy($scope.master, $scope.newCard)
-    $scope.submitted = false;
-    $scope.qRequired = false;
-    $scope.minQLength = false;
-    $scope.newCard.answers.forEach(function(element, index) {
-      $scope["answer" + index + "maxALength"] = false;
-    });
+    angular.copy($scope.master, $scope.newCard);
   }
   $scope.reset();
-  $scope.validate = function() {
-    if($scope.newCardForm.question.$error.required){
-      $scope.qRequired = true;
-      $scope.minQLength = false;
-      flag = true;
-    }else if($scope.newCardForm.question.$error.minlength){
-      $scope.qRequired = false;
-      $scope.minQLength = true;
-      flag = true;
-    }else{
-      $scope.qRequired = false;
-      $scope.minQLength = false;
-      $scope.newCard.answers.forEach(function(answer, index) {
-        if(answer.text === undefined) {
-          flag = true;
-          $scope['answer' + index + "maxALength"] = true;
-        }else{
-          $scope['answer' + index + "maxALength"] = false;
-        }
-      });
-    }
-  }
   $scope.addFlashCard = function(card) {
     if(flag) return;
     FlashCardsFactory.addFlashCard(card).then(function(success) {
